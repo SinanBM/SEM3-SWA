32ee079188be3423a20d7d93404e4115229acf67
Sinan <sinan.ozgur093@gmail.com>
2025-09-15 20:26:44 +0200

    Add coverlet collector

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace HelloWorld
+﻿using System.Net.WebSockets;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.Arm;
+
+namespace HelloWorld
 {
     public static class Hello
     {
 
-        // main method with parameter called args which is type of string 
+        // main method with parameter called args which is type of string
         // method = void; returns nothing
         static void Main(string[] args)
         {
